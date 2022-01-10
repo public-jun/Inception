7e04778304340e7608e1bcce714d4e673416fd85
jnakahod <jnakahod@student.42tokyo.jp>
2022-01-10 03:16:26 +0100

    Add: nginx entrypoint set #1

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
+echo ">> Docker-ENTRYPOINT: GENERATING SSL CERT"
+
+mkdir -p /etc/nginx/ssl/self
+openssl genrsa \
+	-out /etc/nginx/ssl/self/server.key 2048
+openssl req -new \
+	-subj "/C=JP/ST=Tokyo/L=Roppongi/O=42Tokyo/CN=localhost" \
+	-key /etc/nginx/ssl/self/server.key \
+	-out /etc/nginx/ssl/self/server.csr
+openssl x509 -req \
+	-in /etc/nginx/ssl/self/server.csr \
+	-days 3650 \
+	-signkey /etc/nginx/ssl/self/server.key \
+	-out /etc/nginx/ssl/self/server.crt
+
+echo ">> DOCKER-ENTRYPOINT: GERNRATING SSL CERT...DONE"
+echo ">> DOCKER-ENTRYPOINT: EXECUTING CMD"
+
 exec "$@"
