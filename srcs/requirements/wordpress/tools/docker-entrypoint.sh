0c18e4f19debe4eb49a9f67ce24b5ddc51b4b692
jnakahod <jnakahod@student.42tokyo.jp>
2022-01-27 09:00:52 +0100

    Fix: wordpress

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,63 +1,80 @@
 #!/bin/sh
+set -eu
 
-if [ "$(getent group www-data | cut -f 1 -d ":")" = "www-data" ]; then
-	echo "www-data group found"
-else
-	echo "www-data group not found... add now"
-	addgroup -g 82 -S www-data
-	echo "www-data group added"
-fi
+if [ "$1" = '/usr/sbin/php-fpm7' ]; then
+	uid="$(id -u)"
+	gid="$(id -g)"
+	if [ "$uid" = '0' ]; then
+		user='www-data'
+		group='www-data'
+	else
+		user="$uid"
+		group="$gid"
+	fi
 
-if [ "$(getent passwd www-data | cut -f 1 -d ":")" = "www-data" ]; then
-	echo "www-data user found"
-else
-	echo "www-data user not found... add now"
-	adduser -u 82 -D -S -G www-data www-data
-	echo "www-data user added"
-fi
-chown www-data:www-data /var/www/html
+	if [ ! -e "${WORDPRESS_PATH}/index.php" ] && [ ! -e "${WORDPRESS_PATH}/wp=includes/version.php" ]; then
+		if [ "$uid" = '0' ] && [ "$(stat -c '%u:%g' .)" = '0:0' ]; then
+			chown "$user:$group" "${WORDPRESS_PATH}"
+		fi
 
-echo "wordpress download now"
-wp core download --force --path="${WORDPRESS_PATH}" --locale=ja
-chmod -R 777 /var/www/html/wp-content
+		echo >&2 "Wordpress not found in ${WORDPRESS_PATH}... download now"
+		wp core download \
+			--path="${WORDPRESS_PATH}" \
+			--locale=ja \
+			--version=5.8.3
+		if [ $? -eq 0 ]; then
+			chown -R www-data:www-data /var/www/html
+			chmod -R 777 ${WORDPRESS_PATH}/wp-content
+		else
+			exit 1
+		fi
+		echo >&2 "Complete! WordPress has been successfully copied to ${WORDPRESS_PATH}"
 
-echo "Waiting for database"
-until mysql -h"${WORDPRESS_DB_HOST}" -u"${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASSWORD}" &> /dev/null; do
-	>&2 echo -n "."
-	sleep 1
-done
->&2 echo "Database is up"
+		echo "Waiting for database"
+		until mysql -h"${WORDPRESS_DB_HOST}" \
+					-u"${WORDPRESS_DB_USER}" \
+					-p"${WORDPRESS_DB_PASSWORD}" &> /dev/null; do >&2 echo -n "."
+			sleep 1
+		done
+		>&2 echo "Database is up"
 
+		echo "Generating wp-config.php "
+		wp config create \
+			--force \
+			--dbname="${WORDPRESS_DB_NAME}" \
+			--dbuser="${WORDPRESS_DB_USER}" \
+			--dbpass="${WORDPRESS_DB_PASSWORD}" \
+			--dbhost="${WORDPRESS_DB_HOST}" \
+			--path="${WORDPRESS_PATH}"
+		echo "DONE!"
 
-wp config create --force --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --dbhost="${WORDPRESS_DB_HOST}" --path="${WORDPRESS_PATH}"
-echo "config ceate"
-
-echo "Runs the standard WordPress installation process."
-wp core install \
-	--url=https://"${DOMAIN_NAME}" \
-	--title="${TITLE}" \
-	--admin_user="${WORDPRESS_ADMIN_USER}" \
-	--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-	--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
-	--path="${WORDPRESS_PATH}" \
-	--allow-root
-echo "Wordpress INFO
+		echo "Runs the standard WordPress installation process."
+		wp core install \
+			--url=https://"${DOMAIN_NAME}" \
+			--title="${TITLE}" \
+			--admin_user="${WORDPRESS_ADMIN_USER}" \
+			--admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+			--admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+			--path="${WORDPRESS_PATH}" \
+			--allow-root
+		echo "Wordpress INFO
 URL:https://${DOMAIN_NAME}
 TITLR:${TITLE}
 ADMIN_USER:${WORDPRESS_ADMIN_USER}
 ADMIN_PASSWORD:xxx"
 
-echo "Creates a new user."
-wp user create "${WORDPRESS_EDITOR_USER}" \
-	"${WORDPRESS_EDITOR_EMAIL}" \
-	--user_pass="${WORDPRESS_EDITOR_PASSWORD}" \
-	--role="${WORDPRESS_EDITOR_ROLE}" \
-	--path="${WORDPRESS_PATH}" \
-	--allow-root
-echo "New user info
+		echo "Creates a new user."
+		wp user create "${WORDPRESS_EDITOR_USER}" \
+			"${WORDPRESS_EDITOR_EMAIL}" \
+			--user_pass="${WORDPRESS_EDITOR_PASSWORD}" \
+			--role="${WORDPRESS_EDITOR_ROLE}" \
+			--path="${WORDPRESS_PATH}" \
+			--allow-root
+		echo "New user info
 LOGIN:${WORDPRESS_EDITOR_USER}
 ROLE:${WORDPRESS_EDITOR_ROLE}"
 
-chown -R www-data:www-data /var/www/html
+	fi
+fi
 
 exec $@
